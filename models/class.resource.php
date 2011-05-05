481dc23353e01dcd75934da13d124a465a98b364
Aldo Delgado <aldo@osstek.com>
2011-05-05 00:34:46 -0400

    Light naming changes

--- models/class.resource.php
+++ models/class.resource.php
@@ -1,8 +1,8 @@
 <?php
 class Resource extends ResourceBase{
-    var $api_key = SP_API_KEY;
-    var $host = SP_API_URL;
-    var $response_type = SP_FORMAT; #json or xml
+    var $api_key = API_KEY;
+    var $host = API_URL;
+    var $response_type = FORMAT; #json or xml
 }
 
 ?>
